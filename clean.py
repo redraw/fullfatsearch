34ba77aa54f07f08446f0a205181f5590e5f76ac
Agustin Bacigalup <agustinbv@gmail.com>
2024-10-24 23:09:17 -0300

    [deploy] borrar subs vacios

--- clean.py
+++ clean.py
@@ -2,20 +2,13 @@ import os
 import sys
 import sqlite_utils
 
-
 EXCLUDE = [kw.strip() for kw in os.getenv("EXCLUDE", "").split(",") if kw]
-
-if not EXCLUDE:
-    print("nothing to do")
-    sys.exit(0)
-
-db = sqlite_utils.Database(sys.argv[1])
-subs = db["subtitles"]
-
 
 def placeholders(n):
     return ",".join("?" for _ in range(n))
 
+db = sqlite_utils.Database(sys.argv[1])
+subs = db["subtitles"]
 
 for keyword in EXCLUDE:
     ids = [i["rowid"] for i in subs.search(keyword)]
@@ -23,6 +16,9 @@ for keyword in EXCLUDE:
 
     with db.conn:
         subs.delete_where(where, ids, analyze=True)
+
+with db.conn:
+    subs.delete_where("text == ' '", analyze=True)
 
 subs.rebuild_fts()
 subs.optimize()
